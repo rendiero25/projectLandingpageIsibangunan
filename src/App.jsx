2c49ee9bfd3da92089f8b3eef8078f03fb817298
rendiero25 <workspace.rendy@gmail.com>
2025-04-21 20:52:57 +0700

    setting height for iframe wordpress

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 // import Header from "./components/Header";
@@ -11,6 +12,19 @@ import Bannercta from "./components/BannerCTA";
 // import Footer from "./components/Footer";
 
 const App = () => {
+
+  useEffect(() => {
+    const sendHeight = () => {
+      const height = document.body.scrollHeight;
+      window.parent.postMessage({ aiHeight: height }, "*");
+    };
+
+    sendHeight();
+    window.addEventListener("resize", sendHeight);
+
+    return () => window.removeEventListener("resize", sendHeight);
+  }, []);
+
   return(
     <BrowserRouter>
         <div className="">
